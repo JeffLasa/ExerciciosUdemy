6c12d16df5cfbac43e169f38f08775133a9fe503
jeffLasa <jeff.lasa@hotmail>
2019-07-28 19:38:55 -0300

    requisicoesHttp

--- Requisicoes/app/src/main/java/com/example/requisicoes/MainActivity.java
+++ Requisicoes/app/src/main/java/com/example/requisicoes/MainActivity.java
@@ -1,13 +1,126 @@
 package com.example.requisicoes;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoRecuperar;
+    private TextView textoResultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoRecuperar = findViewById(R.id.button_recuperar);
+        textoResultado = findViewById(R.id.textResultado);
+
+        botaoRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTask task = new MyTask();
+
+                String UrlApi = "https://www.blockchain.com/ticker";
+                String cep = "03421000";
+                String urlCep = "https://viacep.com.br/ws/"+ cep +"/json";
+                task.execute(urlCep);
+
+            }
+        });
+
+    }
+
+    class MyTask extends AsyncTask<String,Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String stringUrl = strings[0];
+            InputStream inputStream=null;
+            InputStreamReader inputStreamReader = null;
+            StringBuffer buffer = null;
+
+            try {
+                URL url = new URL(stringUrl);
+                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+
+                //Recupera os dados em Bytes
+                inputStream = conexao.getInputStream();
+
+                //Lê os dados em Bytes e decodifica para caracteree
+                inputStreamReader = new InputStreamReader(inputStream);
+
+                //Objeto utilizado para leitura dos caracteres di InputStreamReader
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                buffer = new StringBuffer();
+                String linha = "";
+
+                while ( (linha = reader.readLine()) !=null){
+                    buffer.append(linha);
+                }
+
+
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+
+            }
+            return buffer.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String resultado) {
+            super.onPostExecute(resultado);
+
+            String logradouro =null;
+            String cep =null;
+            String complemento =null;
+            String bairro =null;
+            String localidade =null;
+            String uf =null;
+
+
+
+            try {
+                JSONObject jsonObject = new JSONObject(resultado);
+                logradouro = jsonObject.getString("logradouro");
+                cep = jsonObject.getString("cep");
+                complemento = jsonObject.getString("complemento");
+                bairro = jsonObject.getString("bairro");
+                localidade = jsonObject.getString("localidade");
+                uf = jsonObject.getString("uf");
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            textoResultado.setText( logradouro+" / "+cep+" / "+complemento+" / "+bairro+" / "+localidade+" / "+uf);
+
+        }
     }
 }
